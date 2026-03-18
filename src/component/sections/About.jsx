a1c4992bb61c37ee6d7165a6c9daaa35b09d6d56
Zolender <ndeingare@gmail.com>
2026-03-18 18:25:50 +0200

    feat: enhance Contact section with form and styling

--- src/component/sections/About.jsx
+++ src/component/sections/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     <p className="font-bold text-lg text-slate-200">Passionate about crafting digital experiences</p>
                     <p className="text-slate-500 text-sm">I am a decicated Full-Stack Developer with a strong focus on building modern, performant web applications. With a solid foundation in both frotend and backend technologies, I enjoy bridging the gap between design and engineering.</p>
                     <p className="text-slate-500 text-sm">My journey in software development is driven by a constant desire to lear and solve complex problems. Whether it's architecting a scalable database or fint-tuning user interfaces for optimal accessibility, I strive for excellence in every line of code I write.</p>
-                    <button className="bg-blue-400 w-fit hover:cursor-pointer text-slate-200 px-5 py-2 rounded-md font-semibold" type="button">Download Resume</button>
+                    <button className="bg-blue-600 w-fit hover:cursor-pointer text-slate-200 px-5 py-2 rounded-md font-semibold" type="button">Download Resume</button>
                 </div>
             </div>
         </div>
